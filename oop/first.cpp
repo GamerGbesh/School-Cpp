74892158333b13abb080faa13c1ab6f2d3a3a95f
Gbesh <gbesh.12@gmail.com>
2025-01-28 12:36:43 +0000

    Working on oop

---
+++ oop/first.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+
+class Car{
+    private:
+        string company;
+        int seatNumber;
+        string color;
+        int wheels;
+        int speed;
+
+    public:
+        Car(string company, int seatNumber, string color, int wheels, int speed){
+            company_setter(company);
+            seat_setter(seatNumber);
+            color_setter(color);
+            wheels_setter(wheels);
+            speed_setter(speed);
+        }
+
+        void company_setter(string company){
+            this->company = company;
+        }
+        string company_getter(){
+            return this->company;
+        }
+
+        void seat_setter(int seatNumber){
+            this->seatNumber = seatNumber;
+        }
+        int seat_getter(){
+            return this->seatNumber;
+        }
+
+        void color_setter(string color){
+            this->color = color;
+        }
+        string color_getter(){
+            return this->color;
+        }
+
+        void wheels_setter(int wheels){
+            this->wheels = wheels;
+        }
+        int wheels_getter(){
+            return this->wheels;
+        }
+
+        void speed_setter(int speed){
+            this->speed = speed;
+        }
+        int speed_getter(){
+            return this->speed;
+        }
+};
+
+
+int main(){
+    Car car("BMw", 2, "Blue", 4, 35);
+    car.company_setter("BMW");
+    cout << car.company_getter() << endl;
+    cout << car.seat_getter() << endl;
+    cout << car.color_getter() << endl;
+    cout << car.wheels_getter() << endl;
+    cout << car.speed_getter() << endl;
+}
